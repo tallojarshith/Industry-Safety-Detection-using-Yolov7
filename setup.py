852c2c683519257813efc8d7988b4099070e756b
Harshith <tallojarshith123@gmail.com>
2024-05-28 16:10:35 +0530

    folder structure added and read me updated

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import find_packages, setup
+
+setup(
+    name = 'isd',
+    version= '0.0.0',
+    author= 'Talloji Harshith',
+    author_email= 'tallojarshith123@gmail.com',
+    packages= find_packages(),
+    install_requires = []
+
+)
